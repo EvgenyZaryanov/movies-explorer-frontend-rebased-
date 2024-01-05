f3d537afdb1a3dad7cbc103c1e6cca390d70df06
Evgeny Zaryanov <eugenezaryanov@yandex.ru>
2024-01-05 03:07:14 +0300

    super-massive update

--- src/utils/constants.jsx
+++
@@ -1,7 +0,0 @@
-const hiddenRoutes = ['/signup', '/signin', '/not-found'];
-
-const moviesUrl = 'https://api.nomoreparties.co/beatfilm-movies';
-
-const baseUrl = 'https://api.zarmovies.nomoredomainsmonster.ru';
-
-export { hiddenRoutes, moviesUrl, baseUrl };
